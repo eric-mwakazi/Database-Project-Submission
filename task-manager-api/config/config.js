c9509b2289cf46ef2cbb8325c0bc00c635c70906
neivany1 <nevilenaivanny@gmail.com>
2025-04-17 16:32:18 +0300

    added finmal rutes

--- task-manager-api/config/config.js
+++ task-manager-api/config/config.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
-
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    }
   }
 };
